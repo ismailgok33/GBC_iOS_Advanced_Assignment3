cb2b5e50e86fe250d02efbcfd2473ebef3a42527
Ismail Gok <ismailgok33@gmail.com>
2022-05-27 11:27:34 -0400

    MKMapView is added and it is showing the capital with a marker

--- A3_Ismail/Controller/CountryDetailsViewController.swift
+++ A3_Ismail/Controller/CountryDetailsViewController.swift
@@ -36,11 +36,17 @@ class CountryDetailsViewController: UIViewController {
     // MARK: - Helpers
     
     private func configureUI() {
-        self.title = country?.name ?? "Details"
+        let title = country?.name ?? "Details"
+        let countryName = country?.name ?? "N/A"
+        let capital = country?.capital ?? "N/A"
+        let countryCode = country?.countryCode ?? "N/A"
+        let area = country?.area ?? 0
         
-        self.countryName.text = country?.name ?? "N/A"
-        self.capital.text = country?.capital ?? "N/A"
-        self.countryCode.text = country?.countryCode ?? "N/A"
+        self.title = title
+        
+        self.countryName.text = countryName
+        self.capital.text = capital
+        self.countryCode.text = countryCode
         if country?.population == -1 {
             self.population.text = "N/A"
         }
@@ -48,7 +54,30 @@ class CountryDetailsViewController: UIViewController {
             self.population.text = "\(country?.population ?? -1)"
         }
         
+        showCapitalOnMap(latlng: country?.latlng ?? [Double](), countryName: countryName, capital: capital, area: area)
+        
         // TODO: set Favorite Button title according to its favorite state
+        
+    }
+    
+    private func showCapitalOnMap(latlng: [Double], countryName: String, capital: String, area: Double) {
+        if latlng.count > 1 {
+            
+            let lat = latlng[0]
+            let lng = latlng[1]
+            
+            let centerOfMapCoordinate = CLLocationCoordinate2D(latitude: lat, longitude: lng)
+            
+            let visibleRegion = MKCoordinateRegion(center: centerOfMapCoordinate, latitudinalMeters: CLLocationDistance(exactly: area)!, longitudinalMeters: CLLocationDistance(exactly: area)!)
+            
+            self.mapView.setRegion(visibleRegion, animated: true)
+            
+            let mapMarker = MKPointAnnotation()
+            mapMarker.coordinate = centerOfMapCoordinate
+            mapMarker.title = "The capital of \(countryName) is \(capital)"
+            self.mapView.addAnnotation(mapMarker)
+        }
+        
         
     }
     
--- A3_Ismail/Model/Country.swift
+++ A3_Ismail/Model/Country.swift
@@ -12,12 +12,16 @@ struct Country: Decodable {
     let capital: String
     let population: Int
     let countryCode: String
+    let latlng: [Double]
+    let area: Double
     
     enum CodingKeys: String, CodingKey {
         case name
         case capital
         case population
         case countryCode = "alpha3Code"
+        case latlng
+        case area
     }
     
     init(from decoder: Decoder) throws {
@@ -27,5 +31,7 @@ struct Country: Decodable {
         self.capital = try countryContainer.decodeIfPresent(String.self, forKey: .capital) ?? "N/A"
         self.population = try countryContainer.decodeIfPresent(Int.self, forKey: .population) ?? -1
         self.countryCode = try countryContainer.decodeIfPresent(String.self, forKey: .countryCode) ?? "N/A"
+        self.latlng = try countryContainer.decodeIfPresent([Double].self, forKey: .latlng) ?? [Double]()
+        self.area = try countryContainer.decodeIfPresent(Double.self, forKey: .area) ?? 0
     }
 }
